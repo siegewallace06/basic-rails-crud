83518f3eb8991c080ba750e9853880d3c879a02a
Farid Muhammad Eryanto <fbrstudio06@gmail.com>
2022-09-23 08:39:22 +0700

    add bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  # About Page
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the about page
+  get 'home/about'
 
   # Defines the root path route ("/")
   root 'home#index'
